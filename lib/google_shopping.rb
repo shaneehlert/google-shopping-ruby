929bcb5743efb455b1f3e96fd062d54b707a9bb4
shaneehlert <shane@esperas.org>
2016-08-30 08:17:11 -0700

    was hard figuring it out but looked at my javascript google shopping and made more sense to me

--- lib/google_shopping.rb
+++ lib/google_shopping.rb
@@ -10,9 +10,58 @@ google_data = JSON.parse(file)
 
 # Use 'inspect' to see the contents of an array or hash
 # Hashes are similar to objects in JavaScript
-puts google_data.inspect
+#puts google_data.inspect
 
 # Hashes require bracket syntax
 # puts google_data['items'][0]['product']['title']
 
 # Put your answers below here
+#1ST ANSWER
+ count = 0
+  google_data["items"].each do |item|
+    if item["kind"] == "shopping#product"
+      count += 1
+    end
+  end
+  puts count
+#ANSWER: 25
+ 
+#2ND ANSWER
+  google_data["items"].each do |item|
+    if item["product"]["inventories"][0]["availability"] == "backorder"
+      puts item
+      count += 1
+    end
+  end
+  puts count
+#3RD ANSWER
+  google_data["items"].each do |item|
+    if item["product"]["images"].length > 1
+      puts item
+      count +=1
+    end
+  end
+  puts count
+#4th ANSWER
+  google_data["items"].each do |item|
+    if item["product"]["brand"].downcase == "canon"
+     puts item
+      count +=1
+   end
+  end
+    puts count
+#5th answer
+    google_data["items"].each do |item|
+    if item["product"]["brand"].downcase == "canon" && item["product"]["author"]["name"] == "eBay"
+      puts item
+      count += 1
+    end
+  end
+  puts count
+ 
+#6th answer
+  google_data["items"].each do |item|
+    puts item["product"]["brand"]
+    puts item["product"]["inventories"][0]["price"]
+    puts item["product"]["images"][0]
+  end
